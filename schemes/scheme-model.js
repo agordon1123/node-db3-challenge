d7488d81933e0143fa473fdf5d79e62897e928db
Alex <agordon1123@gmail.com>
2019-09-11 17:36:17 -0400

    corrected resolution in findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,6 +5,8 @@ module.exports = {
     findById,
     findSteps,
     add,
+    update,
+    remove,
 };
 
 function find() {
@@ -18,7 +20,8 @@ function findById(id) {
 
 function findSteps(id) {
     return db('schemes')
-        .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+    .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+    .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
         .where({ scheme_id: id })
         .then(steps => steps)
         .catch(err => err)
@@ -28,4 +31,16 @@ function add(scheme) {
     return db('schemes').insert(scheme)
         .then(success => success)
         .catch(err => err)
+};
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+};
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del()
 };
