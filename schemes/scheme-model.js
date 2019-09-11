8c60e3f87239650e67ca956d629ebfaa67caf89e
Alex <agordon1123@gmail.com>
2019-09-11 17:07:07 -0400

    created findSteps, add helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,13 +3,29 @@ const db = require('../data/db-config');
 module.exports = {
     find,
     findById,
-}
+    findSteps,
+    add,
+};
 
 function find() {
     return db('schemes')
-}
+};
 
 function findById(id) {
     return db('schemes')
         .where({ id })
-}
+};
+
+function findSteps(id) {
+    return db('schemes')
+        .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+        .where({ scheme_id: id })
+        .then(steps => steps)
+        .catch(err => err)
+};
+
+function add(scheme) {
+    return db('schemes').insert(scheme)
+        .then(success => success)
+        .catch(err => err)
+};
